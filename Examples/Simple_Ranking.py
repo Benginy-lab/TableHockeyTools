178ac5b8275b1d10d6cd6bb8350af7567f7100d3
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2024-11-08 19:32:54 +0100

    Added everything needed for a basic alpha build

---
+++ Examples/Simple_Ranking.py
@@ -0,0 +1,25 @@
+import THTools.THTools as TH
+
+
+    
+if __name__ == "__main__":
+    player_names = ['Evigeny Matansev', 'Rainers Kalnins']
+    player_ids = []
+    for player_name in player_names:
+        player_ids.append(TH.GetPlayerID(player_name))
+    
+    try:
+        print("found player ids = "+str(player_ids))
+    except NameError:
+        print("ERR: Player id not found, check for spelling mistakes in %player_names%")
+        quit()
+    except:
+        print("ERR: something unexpected happened when trying to find player ids")
+        quit()
+    
+        
+    
+
+
+
+
--- setup.py
+++ setup.py
@@ -8,12 +8,19 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',  # Make sure README is in markdown
     author='Benjamin Nygard',           # Your name
-    author_email='your-email@example.com',  # Your email
-    url='https://github.com/username/repo', # Link to your repository
+    author_email='Benjamin.nygard13@gmail.com',  # Your email
+    url='https://github.com/Benginy-lab/TableHockeyTools.git', # Link to your repository
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',            # Minimum Python version
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4',  # The actual name for `bs4` on PyPI
+        'require'
+    ],
 )
