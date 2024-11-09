80415fdc1233f23ed7f37cb3e5985565fd6a3ea4
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2024-11-09 14:16:11 +0100

    added Manifest.in to gitignore and updated setup.py version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_long_description():
 
 setup(
     name='TableHockeyTools',               
-    version='0.1.0',                    
+    version='0.1.1',                    
     packages=find_packages(),           
     description='A collection of tools for working with TableHockey data.',
     long_description=read_long_description(),
