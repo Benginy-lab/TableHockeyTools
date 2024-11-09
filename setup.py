6b5ef1ded6f006d966ddd479375dddaf0d2c8625
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2024-11-09 14:25:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_long_description():
 
 setup(
     name='TableHockeyTools',               
-    version='0.1.1',                    
+    version='0.1.2',                    
     packages=find_packages(),           
     description='A collection of tools for working with TableHockey data.',
     long_description=read_long_description(),
