320c503044e9846f5b1461f674428b8b9823c8e9
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2024-11-12 21:34:22 +0100

    clean up and improve rankingplot

--- Examples/rankingplot.py
+++ Examples/rankingplot.py
@@ -1,31 +1,50 @@
-from TableHockeyTools.THTools import GetPlayerName
 import matplotlib.pyplot as plt
 import time
 import THTools as TH
-from matplotlib.patches import FancyBboxPatch
+from datetime import datetime
 
-# Sample data
-playername = input("input player name: \n")
-playerid = TH.GetPlayerID(playername, return_mode="single")
+
+class player:
+    def __init__(self, playername):
+        self.playername= playername
+        self.playerid = None
+
+        self.data = None
+        self.max_y = None
+
+        self.x_values = []
+        self.y_values = []
+
+    def getdata(self):
+        self.playerid = TH.GetPlayerID(self.playername, return_mode="single")
+
+        self.data = TH.GetHistory(self.playerid, yearago, now, return_mode="dict", supress_warnings=True)
+        if self.data != None:
+            self.x_values = [datetime.strptime(date_str, "%Y-%m") for date_str in self.data.keys()]
+            self.y_values = [int(value) for value in self.data.values()]
+            self.max_y = max(self.y_values)
+            plt.plot(self.x_values, self.y_values, color='blue')
+
 now = time.localtime()
-yearago = f"{now.tm_year - 1}-{now.tm_mon}"
-data = TH.GetHistory(playerid, yearago, now, return_mode="dict")
+yearago = f"{now.tm_year - 4}-{now.tm_mon}"
 
-y_values = list(data.values())
-x_values = [time.mktime(time.strptime(date_str, "%Y-%m")) for date_str in data.keys()]
+player1 = player(input("enter player 1: "))
+player2 = player(input("\nenter player 2: "))
 
-# Create a figure and add a rounded background patch
 
-# Scatter plot
-plt.plot(x_values, y_values, color='blue', marker='o')
 
-# Labeling the plot
-plt.title(f"Plot for {str(TH.GetPlayerName([playerid]))}")
+player1.getdata()
+player2.getdata()
+
+
+plt.ylim(0, max(player1.max_y, player2.max_y)*1.1)
+
+names = TH.GetPlayerName([player1.playerid, player2.playerid], return_mode="list")
+plt.title(f"Plot for {names[0]} and {names[1]}")
 plt.ylabel("Y values")
 plt.xlabel("X values (Date)")
 
 
-# Custom formatting of the y-axis to show dates
 plt.gcf().autofmt_xdate()
-# Display the plot
+
 plt.show()
