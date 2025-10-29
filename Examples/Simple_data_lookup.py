57135761ca60d82cc63c67995f678069c51dd71c
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2025-10-29 22:43:21 +0100

    Updated to use IDPlayer

--- Examples/Simple_data_lookup.py
+++ Examples/Simple_data_lookup.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     player_names = ['Matantsev Evgeniy', 'Kalnins Rainers']
 
     
-    print(TH.GetPlayerID(player_names, return_mode="dict"))
+    print(TH.IDPlayer(player_names, return_mode="dict", direction="N2ID"))
 
     print(TH.GetPlayerRank(player_names=player_names, return_mode="dict", verbose=True))
     
