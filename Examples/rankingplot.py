09f30e5b1eb610c789eb8f803d28dada7202a0ac
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2025-10-29 22:39:23 +0100

    Update to use IDplayer instead of deprecated GetPlayerID

--- Examples/rankingplot.py
+++ Examples/rankingplot.py
@@ -15,10 +15,10 @@ class player:
         self.x_values = []
         self.y_values = []
 
-    def getdata(self):
-        self.playerid = TH.GetPlayerID(self.playername, return_mode="single")
+    def getdata(self, start, stop):
+        self.playerid = TH.IDPlayer(self.playername, return_mode="single", direction="N2ID", verbose=True)
 
-        self.data = TH.GetHistory(self.playerid, yearago, now, return_mode="dict", supress_warnings=True)
+        self.data = TH.GetHistory(self.playerid, start, date_end=stop, getattr="points", return_mode="dict", supress_warnings=False)
         if self.data != None:
             self.x_values = [datetime.strptime(date_str, "%Y-%m") for date_str in self.data.keys()]
             self.y_values = [int(value) for value in self.data.values()]
@@ -26,20 +26,21 @@ class player:
             plt.plot(self.x_values, self.y_values, color='blue')
 
 now = time.localtime()
-yearago = f"{now.tm_year - 4}-{now.tm_mon}"
+yearago = f"{now.tm_year - 10}-{now.tm_mon}"
 
 player1 = player(input("enter player 1: "))
 player2 = player(input("\nenter player 2: "))
 
 
+player1.getdata(yearago, now)
+player2.getdata(yearago, now)
 
-player1.getdata()
-player2.getdata()
+print(player1.data)
 
 
 plt.ylim(0, max(player1.max_y, player2.max_y)*1.1)
 
-names = TH.GetPlayerName([player1.playerid, player2.playerid], return_mode="list")
+names = TH.IDPlayer([player1.playerid, player2.playerid], return_mode="list", direction="ID2N")
 plt.title(f"Plot for {names[0]} and {names[1]}")
 plt.ylabel("Y values")
 plt.xlabel("X values (Date)")
