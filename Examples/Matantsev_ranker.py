035fe3ce9d32c897983b1d068c6499fdbd06a92e
Benginy-lab <154811658+Benginy-lab@users.noreply.github.com>
2025-10-29 22:39:57 +0100

    update to use IDPlayer instead of deprecated GetPlayerID

--- Examples/Matantsev_ranker.py
+++ Examples/Matantsev_ranker.py
@@ -1,6 +1,6 @@
 import THTools as TH
 
-players = TH.GetPlayerID(["Matantsev"], return_mode="list")
+players = TH.IDPlayer("Matantsev", return_mode="list", direction="N2ID", verbose=True)
 PlayerDict = TH.GetPlayerPoints(players, return_mode="dict")
 ranks = TH.LocalRanker(PlayerDict)
 for rank in ranks:
